48290ddd291fc92ea9bca61ff0fa4273e4faea5b
vit <vit@gmail.com>
2020-10-28 23:59:03 +0300

    initial project settings

---
+++ testSetup.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const path = require('path');
+const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
+
+document.body.innerHTML = html;
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    devServer: {
+        port: 3000,
+        index: 'index.html',
+        quiet: true,
+        open: true,
+    }
+}
